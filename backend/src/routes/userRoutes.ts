c054d713afe67245710a3f4cc25f325d4880a944
a4koshin <mankajr11@gmail.com>
2026-05-01 23:55:25 +0300

    chore: update userRoutes.ts

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -18,7 +18,28 @@ router.get(
   roleMiddleware("ADMIN"),
   userController.getAllUsers,
 );
+router.get(
+  "/:id",
+  authMiddleware,
+  roleMiddleware("ADMIN"),
+  userController.getUserById,
+);
+router.put(
+  "/:id",
+  authMiddleware,
+  roleMiddleware("ADMIN"),
+  userController.updateUser,
+);
+router.delete(
+  "/:id",
+  authMiddleware,
+  roleMiddleware("ADMIN"),
+  userController.softDeleteUser,
+);
+
 router.post("/forgot-password", userController.forgotPassword);
 router.post("/reset-password", userController.resetPassword);
+
+router.post("/logout", authMiddleware, userController.logout);
 
 export default router;
