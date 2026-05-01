a6c5c5cdb9615a5e2208f3276fb19cf151d6f4e6
a4koshin <mankajr11@gmail.com>
2026-05-01 23:55:25 +0300

    chore: update index.ts

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,29 @@
 import { Router } from "express";
 import userRoutes from "./userRoutes";
+import doctorRoutes from "./doctorRoutes";
+import availabilityRoutes from "./availabilityRoutes";
+import blockedSlotRoutes from "./blockedSlotRoutes";
+import appointmentRoutes from "./appointmentRoutes";
+import paymentRoutes from "./paymentRoutes";
+import notificationRoutes from "./notificationRoutes";
+import dashboardRoutes from "./dashboardRoutes";
 
 const router = Router();
-
+// Users api
 router.use("/users", userRoutes);
+// Doctor api
+router.use("/doctor", doctorRoutes);
+// Availability Time
+router.use("/availability", availabilityRoutes);
+// Block slot
+router.use("/blocked-slots", blockedSlotRoutes);
+// Appointment
+router.use("/appointments", appointmentRoutes);
+// payment
+router.use("/payments", paymentRoutes);
+// notification
+router.use("/notifications", notificationRoutes);
+// dasboards
+router.use("/dashboard", dashboardRoutes);
 
 export default router;
